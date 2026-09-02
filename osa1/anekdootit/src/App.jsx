8814e1d9c57774657aadc908fbfc69347b22cc52
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-02 16:15:16 +0300

    tehtävä 1.13

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -22,16 +22,26 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
 
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
   const [selected, setSelected] = useState(0)
 
   const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  const voteAnecdote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>Has {votes[selected]} votes.</p>
       <Button onClick={randomAnecdote} text={"New anecdote"}/>
+      <Button onClick={voteAnecdote} text={"Vote"}/>
     </div>
   )
 }
