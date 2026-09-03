d8f7fd985a24e15b1a7fd3a6955fe5ec7d439ca6
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-03 16:13:17 +0300

    tehtävä 2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -6,16 +6,23 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const [newNumber, setNewNumber] = useState(0)
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook!`)
     } else {
       setPersons(persons.concat(nameObject))
     }
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleNameChange = (event) => {
@@ -26,16 +33,13 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleNameChange} /></div>
+        <div>number: <input onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       )}
     </div>
   )
