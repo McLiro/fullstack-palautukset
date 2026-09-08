db5082aeca77a276d181de4d7cd93ba9cac0c001
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-08 15:38:20 +0300

    tehtävä 3.1

---
+++ osa3/puhelinluettelon-backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-204214"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-55-2123123"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-245235"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
