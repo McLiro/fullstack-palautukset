e7e1dff5f2ff0c4ce219c6e56ff39953c35224d2
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-08 15:56:32 +0300

    tehtävä 3.3

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -3,17 +3,17 @@ const app = express()
 
 const persons = [
     {
-        id: 1,
+        id: "1",
         name: "Arto Hellas",
         number: "040-204214"
     },
     {
-        id: 2,
+        id: "2",
         name: "Ada Lovelace",
         number: "39-55-2123123"
     },
     {
-        id: 3,
+        id: "3",
         name: "Dan Abramov",
         number: "12-43-245235"
     }
@@ -21,6 +21,17 @@ const persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
