ffce90ce1cb870bfa887fb7c1559df28b6d83a38
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-14 17:20:18 +0300

    tehtävä 3.15

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -70,10 +70,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
+  const id = request.params.id
+  Person.findByIdAndDelete(id).then(result => {
     response.status(204).end()
+  })
 })
 
 app.get('/info', (request, response) => {
