f21daa7099cac295cb907e841fdd113556a45561
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-14 16:15:41 +0300

    tehtävä 3.12

---
+++ osa3/puhelinluettelon-backend/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.wbpo7th.mongodb.net/Persons?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => console.log(`${person.name} ${person.number}`))
+    mongoose.connection.close()
+  })
+} else {
+  const newPerson = new Person({
+    name: name,
+    number: number,
+  })
+
+  newPerson.save().then(result => {
+    console.log(`Added ${newPerson.name} number ${newPerson.number} to phonebook.`)
+    mongoose.connection.close()
+  })
+}
