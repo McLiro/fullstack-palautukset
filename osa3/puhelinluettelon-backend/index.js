3dc84ac0daf11af5137c65650d99653db745135c
Iiro Suomalainen <suomalainen.iiro@gmail.com>
2026-09-14 16:58:25 +0300

    tehtävä 3.13

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -33,7 +34,9 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
